7bc2baee44928975eed42a88b3bc6d472f58fed8
yupeng-du <ydu6@rrc.ca>
2026-01-25 16:40:54 -0600

    feat: update controller and use HTTP constants.

--- src/api/v1/controllers/ticketContoller.ts
+++ src/api/v1/controllers/ticketContoller.ts
@@ -1,4 +1,21 @@
 import { Request, Response } from "express";
+import { HTTP_STATUS } from "../../../constants/httpConstants";
 import * as ticketService from "../services/ticketService";
 
-export const getAllTickets = async
+export const getAllTickets = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {
+  try {
+    const tickets: ticketService.Ticket[] = await ticketService.getAllTickets();
+    res
+      .status(HTTP_STATUS.OK)
+      .json({ message: "Get all tickets", data: tickets });
+  } catch (error) {
+    console.error("Error fetching tickets:", error);
+    res
+      .status(HTTP_STATUS.INTERNAL_SERVER_ERROR)
+      .json({ message: "Internal Server Error" });
+  }
+};
+
